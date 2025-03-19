12316dfe9c80ae28fab1c0d82e3d0c432c6640f0
ElieAssaf33 <elieassaflt@gmail.com>
2025-03-19 22:08:43 +0200

    navigation integrated

--- photography/photo/urls.py
+++ photography/photo/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('portfolio',views.portfolio,name='portfolio'),
+    path('prices',views.prices,name='prices'),
+    path('contacts',views.contacts,name='contacts') 
 ]
--- photography/photo/views.py
+++ photography/photo/views.py
@@ -5,3 +5,15 @@ from . import models
 def index(request:HttpRequest):
     context = {'photos' : models.Photos.objects}
     return render(request,'photo/index.html', context)
+
+def portfolio(request:HttpRequest):
+    return render(request,'photo/portfolio.html', context={})
+
+def prices(request:HttpRequest):
+    return render(request,'photo/prices.html', context={})
+
+def contacts(request:HttpRequest):
+    return render(request,'photo/contacts.html', context={})
+
+
+
